a3c1fd67926177f99f96056bd42a10a1cd4f8070
Alexander <mail@fomalhaut.su>
2023-06-03 11:53:08 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-"""
-To make release:
-    python setup.py sdist
-Installation:
-    python setup.py install
-"""
 from setuptools import find_packages, setup
 
 from gui_args_framework.version import __version__
+
+
+with open('README.md') as f:
+    long_description = f.read()
 
 
 setup(
     name='gui_args_framework',
     version=__version__,
     packages=find_packages(),
-    license=open('LICENSE.txt').read(),
+    license="Free",
     description="A framework to create a GUI for a Python console application",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
         'PyQt5>=5.15',
